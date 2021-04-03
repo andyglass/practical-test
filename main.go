2683f2b2cde3ba3acf0b268dbd928654fedc6ba8
andyglass <markov.andrey.v@gmail.com>
2021-04-03 16:06:20 +0300

    Hold changes

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"io"
-	"log"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
@@ -15,17 +16,43 @@ import (
 )
 
 type Config struct {
+	isMultiThread   bool
+	Uri             string
 	BucketName      string
 	Prefix          string
 	DestinationPath string
 }
 
 type Storage struct {
+	Ctx    context.Context
 	Client *storage.Client
+	Config *Config
 }
 
 func NewConfig() *Config {
-	return &Config{}
+	isMultiThread := flag.Bool("m", false, "Run command in multi-thread mode")
+	flag.Parse()
+
+	argLen := len(flag.Args())
+	if argLen != 2 {
+		exception(fmt.Errorf("unexpected arguments count: %d instead of 2", argLen))
+	}
+
+	uri := flag.Arg(0)
+	destinationPath := flag.Arg(1)
+
+	bucketName, prefix, err := parseGCSUrl(uri)
+	if err != nil {
+		exception(err)
+	}
+
+	return &Config{
+		isMultiThread:   *isMultiThread,
+		Uri:             uri,
+		BucketName:      bucketName,
+		Prefix:          prefix,
+		DestinationPath: destinationPath,
+	}
 }
 
 func NewStorage() *Storage {
@@ -33,21 +60,22 @@ func NewStorage() *Storage {
 	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		log.Fatal(err)
+		exception(err)
 	}
 
 	return &Storage{
+		Ctx:    ctx,
 		Client: client,
+		Config: NewConfig(),
 	}
 }
 
-func (s *Storage) ListObjects(bucket, prefix string) ([]string, error) {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Second*30)
+func (s *Storage) ListObjects() ([]string, error) {
+	ctx, cancel := context.WithTimeout(s.Ctx, time.Second*30)
 	defer cancel()
 
-	it := s.Client.Bucket(bucket).Objects(ctx, &storage.Query{
-		Prefix: prefix,
+	it := s.Client.Bucket(s.Config.BucketName).Objects(ctx, &storage.Query{
+		Prefix: s.Config.Prefix,
 	})
 
 	var objects []string
@@ -61,21 +89,25 @@ func (s *Storage) ListObjects(bucket, prefix string) ([]string, error) {
 		}
 		objects = append(objects, attrs.Name)
 	}
+
+	if len(objects) == 0 {
+		return nil, fmt.Errorf("no URLs matched: %s", s.Config.Uri)
+	}
+
 	return objects, nil
 }
 
-func (s *Storage) DownloadObject(bucket, object, rootPath string) error {
-	ctx := context.Background()
-	ctx, cancel := context.WithTimeout(ctx, time.Second*60)
+func (s *Storage) DownloadObject(object string) error {
+	ctx, cancel := context.WithTimeout(s.Ctx, time.Second*60)
 	defer cancel()
 
-	sr, err := s.Client.Bucket(bucket).Object(object).NewReader(ctx)
+	sr, err := s.Client.Bucket(s.Config.BucketName).Object(object).NewReader(ctx)
 	if err != nil {
 		return fmt.Errorf("Object(%q).NewReader: %v", object, err)
 	}
 	defer sr.Close()
 
-	fpath := filepath.Join(rootPath, object)
+	fpath := filepath.Join(s.Config.DestinationPath, object)
 
 	err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm)
 	if err != nil {
@@ -88,63 +120,69 @@ func (s *Storage) DownloadObject(bucket, object, rootPath string) error {
 	}
 	defer out.Close()
 
+	fmt.Printf("Copying %s => %s\n", object, fpath)
+
 	_, err = io.Copy(out, sr)
 	if err != nil {
 		return fmt.Errorf("io.Copy: %v", err)
 	}
 
-	log.Printf("%s => %s", object, fpath)
-
 	return nil
 }
 
-func parseGCSUrl(uri string) (string, error) {
-	const prefix = "gs://"
+func parseGCSUrl(uri string) (string, string, error) {
+	const scheme = "gs://"
 
-	if !strings.HasPrefix(uri, prefix) {
-		return "", fmt.Errorf("parse GCS URI %q: scheme must be %q", uri, prefix)
+	if !strings.HasPrefix(uri, scheme) {
+		return "", "", fmt.Errorf("scheme must be \"%s\": %s", scheme, uri)
 	}
 
-	object := uri[len(prefix):]
-	if strings.IndexByte(object, '/') == -1 {
-		return "", fmt.Errorf("parse GCS URI %q: no object name", uri)
+	u, err := url.Parse(uri)
+	if err != nil {
+		return "", "", fmt.Errorf("could not parse uri: %s", uri)
 	}
 
-	return object, nil
+	bucket := u.Host
+	if bucket == "" {
+		return "", "", fmt.Errorf("could not parse bucket name: %s", uri)
+	}
+
+	path := u.Path
+	if path != "" {
+		path = strings.Replace(path, "/", "", 1)
+	}
+
+	return bucket, path, nil
+}
+
+func exception(err error) {
+	fmt.Printf("CommandException: %v\n", err)
+	os.Exit(1)
 }
 
 /*
 	TODO:
-		- Return Bucket name and object prefix path separately
-		- Read config from command line arguments
 		- Optional: MultiThreading (-m)
 */
 
 func main() {
 
-	bucketName := "online-infra-engineer-test"
-	prefix := "mydir"
-	path := "folder/newdir"
-
-	// log.Print(parseGCSUrl("gs://online-infra-engineer-test/mydir"))
+	// url := "gs://online-infra-engineer-test/myd"
 
 	storage := NewStorage()
 	defer storage.Client.Close()
 
-	objects, err := storage.ListObjects(bucketName, prefix)
+	objects, err := storage.ListObjects()
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	if len(objects) == 0 {
-		log.Print("Objects not found")
-		return
+		exception(err)
 	}
 
 	for _, obj := range objects {
-		err := storage.DownloadObject(bucketName, obj, path)
+		err := storage.DownloadObject(obj)
 		if err != nil {
-			log.Print(err)
+			exception(err)
 		}
 	}
+
+	fmt.Printf("Operation completed over %d objects.\n", len(objects))
 }
